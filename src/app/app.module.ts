a2bfa99a63c12332811a9f9580b75702f3da57b0
bearnithi <bearnithi@gmail.com>
2019-08-10 18:00:33 +0530

    font-awesome included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BnNgTreeModule } from 'projects/bn-ng-tree/src/public_api';
+/** DEV */
+// import { BnNgTreeModule } from 'projects/bn-ng-tree/src/public_api';
+import { BnNgTreeModule} from 'bn-ng-tree-lib';
 
 @NgModule({
   declarations: [
